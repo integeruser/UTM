702a62c274d67848f0a6f550f2d799befb700502
Francesco Cagnin <fcagnin@quarkslab.com>
2022-04-22 13:31:59 +0200

    feat: add helpers for patching the entitlements check

---
+++ resume.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+while :
+do
+    # assuming only one VM running
+    pid=$(pgrep com.apple.Virtualization.VirtualMachine)
+    if [ -n "$pid" ]; then break; fi
+    sleep 1
+done    
+
+lldb -b -p $pid -s <(echo "
+b xpc_connection_copy_entitlement_value
+breakpoint command add
+    thread return (id)xpc_bool_create(1)
+    c
+DONE
+b main
+breakpoint command add
+    detach
+DONE
+c
+")
